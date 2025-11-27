acc9a3c762fa87e82b3b683eeb71111058bb608b
dicke <accountmail@gmx.de>
2025-11-27 23:22:00 +0100

    i3clock-color initial setup

--- scripts/scripts/lock.sh
+++ scripts/scripts/lock.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
 
-# 1. Take a screenshot (Fast)
-#scrot /tmp/screenshot.png
+# --- Adapta-Nokto Color Palette ---
+BG="222D32"   # Deep Dark Grey (Background)
+RING="00BCD4" # Cyan (The Ring)
+TEXT="CFD8DC" # Light Grey (Text)
+WRONG="FF5252" # Red (Wrong Password)
+VERIF="00BCD4" # Cyan (Verifying)
+TYPING="69F0AE" # Material Green (Key Press)
 
-# 2. The "Scale" Trick (Fast Blur)
-# We shrink it to 5% (pixelating it) then blow it back up.
-#convert /tmp/screenshot.png -scale 5% -scale 2000% /tmp/screenshotblur.png
-
-# 3. Lock
-#i3lock -i /tmp/screenshotblur.png
-
-i3lock -c 222D32 --no-unlock-indicator
+i3lock \
+--color=$BG \
+--inside-color=$BG \
+--ring-color=$RING \
+--line-uses-inside \
+--keyhl-color=$TYPING \
+--bshl-color=$WRONG \
+--separator-color=$BG \
+--verif-color=$BG \
+--wrong-color=$WRONG \
+--layout-color=$TEXT \
+--date-color=$TEXT \
+--time-color=$TEXT \
+--screen 1 \
+--clock \
+--indicator \
+--time-str="%H:%M" \
+--date-str="%d.%m.%Y" \
+--verif-text="Verifying..." \
+--wrong-text="Wrong!" \
+--no-modkey-text \
+--radius=220 \
+--ring-width=20 \
+--time-size=110 \
+--date-size=40 \
+--layout-size=50 \
+--verif-size=50 \
+--wrong-size=50 \
+--time-pos="ix:iy-20" \
+--date-pos="tx:ty+110"
