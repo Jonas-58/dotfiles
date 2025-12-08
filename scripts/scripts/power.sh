51d596e7ef1f3ba11417468d30ea0b66d8d49275
dicke <accountmail@gmx.de>
2025-12-08 23:58:03 +0100

    power menu move awake to top

--- scripts/scripts/power.sh
+++ scripts/scripts/power.sh
@@ -45,7 +45,7 @@ theme_str='
 '
 
 # 3. Pass the list to Rofi
-chosen="$(echo -e "$suspend\n$shutdown\n$coffee\n$reboot\n$logout\n$lock" | rofi -dmenu -theme-str "${theme_str}")"
+chosen="$(echo -e "$coffee\n$suspend\n$shutdown\n$reboot\n$logout\n$lock" | rofi -dmenu -theme-str "${theme_str}")"
 
 # 4. Execute the command
 case $chosen in
